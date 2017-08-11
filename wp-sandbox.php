0d0b51edd7196435abdfcd91f9c744d0f0d612fd
MD SAMI <mdsami.diu@gmail.com>
2017-08-11 21:31:46 +0600

    add sanbox user role

--- wp-sandbox.php
+++ wp-sandbox.php
@@ -17,3 +17,23 @@ defined( 'ABSPATH' ) or die( 'Access not allowed!' );
 
 //testing time for  users in sec
 $wpSandbocSec = get_option('sandboxTime',20)*60;
+
+
+//Add Action/filter Hook
+register_activation_hook(__FILE__,'wp_sandbox_activation');
+function wp_sandbox_activation(){
+    global $wp_roles;
+    if ( ! isset( $wp_roles ) ){
+        $wp_roles = new WP_Roles();
+    }
+
+    $adm = $wp_roles->get_role('administrator');
+
+    // Adding a new role with all admin caps.
+    $wp_roles->add_role('sandbox-user', 'My Custom Role', $adm->capabilities);
+
+
+
+
+
+}
