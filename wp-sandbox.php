82bff450398eefda96c777553915688fa1154e71
MD SAMI <mdsami.diu@gmail.com>
2017-08-11 21:09:01 +0600

    wp Sandbox

---
+++ wp-sandbox.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Wp Sandbox
+Plugin URI: http://wincred.com/plugins
+Description: Allows non-logged in users to view your wordpress admin area,
+Version: 1.0
+Author: mdsami
+Author URI: http://mdsami.me
+License: A "Slug" license name e.g. GPL2
+*/
+
+
+// Prohibit direct file accessing.
+defined( 'ABSPATH' ) or die( 'Access not allowed!' );
